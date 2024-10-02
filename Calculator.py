cb9d70e829da4c662feac98d4a54a07d3868e2fb
Dmitry Burlyaev <dm.burlyaev@gmail.com>
2024-10-02 23:19:56 +0300

    Homework Assignment 2: Building and Testing with GitHub Actions- start v1.6

--- Calculator.py
+++ Calculator.py
@@ -1,29 +1,19 @@
 class Calculator:
-
 	#empty constructor
-
 	def __init__(self):
     		pass
-
 #add method - given two numbers, return the addition
-
 	def add(self, x, y):
     	return x + y
-  
-#multiply method - given two numbers, return the 
-  
+#multiply method - given two numbers, return the  
 #multiplication of the two
   	def multiply(self, x, y):
     	return x * y
- 
  #subtract method - given two numbers, return the value
-
  #of first value minus the second
   	def subtract(self, x, y):
     	return x - y
-
  #divide method - given two numbers, return the value
- 
  #of first value divided by the second
   	def divide(self, x, y):
     	if y != 0:
