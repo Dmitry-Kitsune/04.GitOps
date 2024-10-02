316cc29acee910809a8aaf16d20490151587ea71
Dmitry Burlyaev <dm.burlyaev@gmail.com>
2024-10-02 23:18:26 +0300

    Homework Assignment 2: Building and Testing with GitHub Actions- start v1.6

--- TestCalculator.py
+++ TestCalculator.py
@@ -2,43 +2,18 @@ import unittest
 from Calculator import Calculator
 
 class TestCalculator(unittest.TestCase):
-
-    def setUp(self):
-        self.calculator = Calculator()
-
-    def test_divide(self):
-
-        self.assertEqual(self.calculator.divide(10, 5), 2)
-        self.assertEqual(self.calculator.divide(-10, 5), -2)
-        self.assertEqual(self.calculator.divide(10, -5), -2)
-        self.assertRaises(ZeroDivisionError, self.calculator.divide, 10, 0)
-        self.assertRaises(TypeError, self.calculator.divide, 10, "5")
-        self.assertRaises(TypeError, self.calculator.divide, "10", 5)
-
-    def test_multiply(self):
-
-        self.assertEqual(self.calculator.multiply(10, 5), 50)
-        self.assertEqual(self.calculator.multiply(-10, 5), -50)
-        self.assertEqual(self.calculator.multiply(10, -5), -50)
-        self.assertRaises(TypeError, self.calculator.multiply, 10, "5")
-        self.assertRaises(TypeError, self.calculator.multiply, "10", 5)
-
-    def test_sum(self):
-
-        self.assertEqual(self.calculator.sum(10, 5), 15)
-        self.assertEqual(self.calculator.sum(-10, 5), -5)
-        self.assertEqual(self.calculator.sum(10, -5), 5)
-        self.assertRaises(TypeError, self.calculator.sum, 10, "5")
-        self.assertRaises(TypeError, self.calculator.sum, "10", 5)
-
-    def test_subtract(self):
-
-        self.assertEqual(self.calculator.subtract(10, 5), 5)
-        self.assertEqual(self.calculator.subtract(-10, 5), -15)
-        self.assertEqual(self.calculator.subtract(10, -5), 15)
-        self.assertRaises(TypeError, self.calculator.subtract, 10, "5")
-        self.assertRaises(TypeError, self.calculator.subtract, "10", 5)
-
-
+  #setUp method is overridden from the parent class TestCase
+  def setUp(self):
+    self.calculator = Calculator()
+  #Each test method starts with the keyword test_
+  def test_add(self):
+    self.assertEqual(self.calculator.add(3,5), 8)
+  def test_subtract(self):
+    self.assertEqual(self.calculator.subtract(7,2), 5)
+  def test_multiply(self):
+    self.assertEqual(self.calculator.multiply(4,8), 32)
+  def test_divide(self):
+    self.assertEqual(self.calculator.divide(25,5), 5)
+# Executing the tests in the above test case class
 if __name__ == "__main__":
-    unittest.main()
+  unittest.main()
